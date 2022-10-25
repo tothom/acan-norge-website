761f2b59209a5448706d2f23b97f495fef27ffd6
tothom <mail@thomasholth.no>
2022-10-25 20:59:46 +0200

    More debug

--- site/templates/debug.php
+++ site/templates/debug.php
@@ -3,9 +3,9 @@
 
 <h3><?= $page->title()->esc() ?></h3>
 
-
 <body>
-<div>
-  <?= dump(F::isWritable(kirby()->root('media'))) ?>
-</div>
+  <div>
+    <?= dump(F::isWritable(kirby()->root('media'))) ?>
+    <?= phpinfo() ?>
+  </div>
 </body>
